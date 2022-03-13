919e9337f19bc7ef9ff54dc0a22f612033522276
Dayoung Kim <kimdayoung771@gmail.com>
2022-03-13 23:58:08 +0900

    apply CounterContainer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Counter from "./components/Counter";
 import Todos from "./components/Todos";
+import CounterContainer from "./containers/CounterContainer";
 
 function App() {
   return (
     <div>
-      <Counter number={ 0 }/>
+      <CounterContainer number={ 0 }/>
       <hr/>
       <Todos/>
     </div>
--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Counter from "../components/Counter";
-import connect from 'react-redux'
+import {connect} from 'react-redux'
 
 const CounterContainer = ({ number, increase, decrease }) => {
   return (
